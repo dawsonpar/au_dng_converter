6da418d654d6c3da7ef4b7ee2177c61286c785c7
Dawson Par <dawson@andrews.edu>
2023-09-04 16:42:55 -0400

    move_dng function and updated get_from_dropbox function

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
+import shutil
 import time
-
-# open -a /Applications/Adobe\ DNG\ Converter.app --args -c /path/to/photo
-# open -a /Applications/Adobe\ DNG\ Converter.app --args -c /Users/dawsonpar/Desktop/coding/AU/20230126_0162_dp.cr3
 
 
 def get_from_dropbox():
     """
     Looks for dropbox folder inside of current directory
     Finds raw photos inside dropbox folder
+    Checks if dng conversion already exists
     Returns:
         raw_photo_paths : set with paths to raw photos
     """
@@ -17,35 +16,51 @@ def get_from_dropbox():
     script_directory = os.path.dirname(os.path.abspath(__file__))
 
     dropbox_folder = "dropbox"
+    converted_folder = "converted"
 
     subdirectory_path = os.path.join(script_directory, dropbox_folder)
+    converted_directory_path = os.path.join(script_directory, converted_folder)
 
     raw_photo_paths = set()
 
     # Check if the subdirectory exists
     if os.path.exists(subdirectory_path) and os.path.isdir(subdirectory_path):
-        # List all files with the ".cr3" extension in the "dropbox" directory
-        cr3_files = [
+        extensions_to_search = [".cr3", ".cr2", ".arw", ".nef"]
+        matching_files = [
             filename
             for filename in os.listdir(subdirectory_path)
-            if filename.endswith(".cr3")
+            if any(filename.endswith(ext) for ext in extensions_to_search)
         ]
 
-        if cr3_files:
+        if matching_files:
             print(
-                f"Found {len(cr3_files)} '.cr3' files in the '{dropbox_folder}' directory:"
+                f"Found {len(matching_files)} files with the specified extensions in the '{dropbox_folder}' directory:"
             )
-            for cr3_file in cr3_files:
-                # print(os.path.join(subdirectory_path, cr3_file))
-                raw_photo_paths.add(os.path.join(subdirectory_path, cr3_file))
+            for matching_file in matching_files:
+                raw_photo_path = os.path.join(subdirectory_path, matching_file)
+
+                # Check if the corresponding .dng file exists in the "converted" directory
+                dng_filename = os.path.splitext(matching_file)[0] + ".dng"
+                dng_path = os.path.join(converted_directory_path, dng_filename)
+
+                if not os.path.exists(dng_path):
+                    print(raw_photo_path)
+                    raw_photo_paths.add(raw_photo_path)
+                else:
+                    print(
+                        f"Skipping {raw_photo_path} as it has already been converted."
+                    )
+
         else:
-            print(f"No '.cr3' files found in the '{dropbox_folder}' directory.")
+            print(
+                f"No files with the specified extensions found in the '{dropbox_folder}' directory."
+            )
+
     else:
         print(
             f"The '{dropbox_folder}' directory does not exist in the script's directory."
         )
 
-    print(raw_photo_paths)
     return raw_photo_paths
 
 
@@ -61,18 +76,62 @@ def convert_to_dng(source_path):
     dng_args = "-c"
 
     os.system(open_dng + " " + dng_args + " " + source_path)
-    print("Converted photo")
+    print(f"Converted {source_path}")
+
+
+def move_dngs():
+    """
+    Finds all files with .dng extension in dropbox directory
+    Moves .dng files to converted directory if it exists
+    """
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    source_directory_name = "dropbox"
+    destination_directory_name = "converted"
+
+    source_directory_path = os.path.join(script_directory, source_directory_name)
+    destination_directory_path = os.path.join(
+        script_directory, destination_directory_name
+    )
+
+    if os.path.exists(source_directory_path) and os.path.isdir(source_directory_path):
+        if not os.path.exists(destination_directory_path):
+            os.makedirs(destination_directory_path)
+
+        dng_files = [
+            filename
+            for filename in os.listdir(source_directory_path)
+            if filename.endswith(".dng")
+        ]
+
+        for dng_file in dng_files:
+            source_file_path = os.path.join(source_directory_path, dng_file)
+            destination_file_path = os.path.join(destination_directory_path, dng_file)
+            shutil.move(source_file_path, destination_file_path)
+
+        print(
+            f"Moved {len(dng_files)} .dng files to the '{destination_directory_name}' directory."
+        )
+    else:
+        print(
+            f"The '{source_directory_name}' directory does not exist in the script's directory."
+        )
 
 
 def main():
-    # path_to_photos = "/Users/dawsonpar/Desktop/coding/AU/20230126_0167_dp.cr3"
-    # convert_to_dng(path_to_photos)
     photo_paths = get_from_dropbox()
 
     for path in photo_paths:
-        convert_to_dng(path)
-        print("Converted" + path)
         time.sleep(1)
+        convert_to_dng(path)
+
+    if len(photo_paths) != 0:
+        print("Preparing to move dng files.")
+        time.sleep(3)
+
+        move_dngs()
+    else:
+        print("No files where moved.")
 
 
 if __name__ == "__main__":
