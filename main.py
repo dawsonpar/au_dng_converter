f96f902ca7ad616104a8a3689977b1488cd0c8e4
Dawson Par <dawson@andrews.edu>
2023-09-06 10:59:32 -0400

    added watchdog

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
 import os
 import shutil
+import subprocess
+import sys
 import time
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+extensions_to_search = [".cr3", ".cr2", ".arw", ".nef"]
+
+dropbox_folder = "dropbox"
+converted_folder = "converted"
+raw_folder = "raws"
+
+subdirectory_path = os.path.join(script_directory, dropbox_folder)
+
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            return
+
+        file_path = event.src_path
+
+        if any(file_path.endswith(ext) for ext in extensions_to_search):
+            print(f"New file detected: {file_path}")
+
+            photo_paths = get_from_dropbox()
+
+            for path in photo_paths:
+                time.sleep(1)
+                convert_to_dng(path)
+
+            if len(photo_paths) != 0:
+                print("Preparing to move dng files.")
+                time.sleep(3)
+
+                # move dngs to converted folder
+                move_photos()
+
+                # move raws to raws folder
+                move_photos(dng=False)
+            else:
+                # move any remaingin raws to raws folder
+                move_photos(dng=False)
 
 
 def get_from_dropbox():
@@ -12,20 +56,12 @@ def get_from_dropbox():
         raw_photo_paths : set with paths to raw photos
     """
 
-    # Get the current directory where the script is located
-    script_directory = os.path.dirname(os.path.abspath(__file__))
-
-    dropbox_folder = "dropbox"
-    converted_folder = "converted"
-
-    subdirectory_path = os.path.join(script_directory, dropbox_folder)
     converted_directory_path = os.path.join(script_directory, converted_folder)
 
     raw_photo_paths = set()
 
     # Check if the subdirectory exists
     if os.path.exists(subdirectory_path) and os.path.isdir(subdirectory_path):
-        extensions_to_search = [".cr3", ".cr2", ".arw", ".nef"]
         matching_files = [
             filename
             for filename in os.listdir(subdirectory_path)
@@ -50,6 +86,7 @@ def get_from_dropbox():
                     print(
                         f"Skipping {raw_photo_path} as it has already been converted."
                     )
+                    move_photos(dng=False)
 
         else:
             print(
@@ -79,59 +116,103 @@ def convert_to_dng(source_path):
     print(f"Converted {source_path}")
 
 
-def move_dngs():
+def move_raws():
     """
-    Finds all files with .dng extension in dropbox directory
-    Moves .dng files to converted directory if it exists
+    Finds remaining raw files in dropbox directory
+    Moves those files into raws directory
     """
-    script_directory = os.path.dirname(os.path.abspath(__file__))
 
-    source_directory_name = "dropbox"
-    destination_directory_name = "converted"
+    source_directory_name = dropbox_folder
+    destination_directory_name = raw_folder
 
     source_directory_path = os.path.join(script_directory, source_directory_name)
     destination_directory_path = os.path.join(
         script_directory, destination_directory_name
     )
 
-    if os.path.exists(source_directory_path) and os.path.isdir(source_directory_path):
-        if not os.path.exists(destination_directory_path):
-            os.makedirs(destination_directory_path)
 
-        dng_files = [
-            filename
-            for filename in os.listdir(source_directory_path)
-            if filename.endswith(".dng")
-        ]
+def move_photos(dng=True):
+    """
+    Finds all files with .dng extension in dropbox directory
+    Moves .dng files to converted directory if it exists
+    Args:
+        dng : boolean to move raw or dng files
+    """
+    source_directory_name = dropbox_folder
+    destination_directory_name = converted_folder
 
-        for dng_file in dng_files:
-            source_file_path = os.path.join(source_directory_path, dng_file)
-            destination_file_path = os.path.join(destination_directory_path, dng_file)
-            shutil.move(source_file_path, destination_file_path)
+    source_directory_path = os.path.join(script_directory, source_directory_name)
+    destination_directory_path = os.path.join(
+        script_directory, destination_directory_name
+    )
 
-        print(
-            f"Moved {len(dng_files)} .dng files to the '{destination_directory_name}' directory."
-        )
+    if dng:
+        if os.path.exists(source_directory_path) and os.path.isdir(
+            source_directory_path
+        ):
+            if not os.path.exists(destination_directory_path):
+                os.makedirs(destination_directory_path)
+
+            dng_files = [
+                filename
+                for filename in os.listdir(source_directory_path)
+                if filename.endswith(".dng")
+            ]
+
+            for dng_file in dng_files:
+                source_file_path = os.path.join(source_directory_path, dng_file)
+                destination_file_path = os.path.join(
+                    destination_directory_path, dng_file
+                )
+                shutil.move(source_file_path, destination_file_path)
+
+            print(
+                f"Moved {len(dng_files)} .dng files to the '{destination_directory_name}' directory."
+            )
+        else:
+            print(
+                f"The '{source_directory_name}' directory does not exist in the script's directory."
+            )
     else:
-        print(
-            f"The '{source_directory_name}' directory does not exist in the script's directory."
+        destination_directory_name = raw_folder
+        destination_directory_path = os.path.join(
+            script_directory, destination_directory_name
         )
+
+        if os.path.exists(source_directory_path) and os.path.isdir(
+            source_directory_path
+        ):
+            if not os.path.exists(destination_directory_path):
+                os.makedirs(destination_directory_path)
+
+            files = [filename for filename in os.listdir(source_directory_path)]
+
+            for f in files:
+                source_file_path = os.path.join(source_directory_path, f)
+                destination_file_path = os.path.join(destination_directory_path, f)
+                shutil.move(source_file_path, destination_file_path)
+
+            print(
+                f"Moved {len(files)} files to the '{destination_directory_name}' directory."
+            )
+        else:
+            print(
+                f"The '{source_directory_name}' directory does not exist in the script's directory."
+            )
 
 
 def main():
-    photo_paths = get_from_dropbox()
+    event_handler = FileEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=subdirectory_path, recursive=False)
+    observer.start()
 
-    for path in photo_paths:
-        time.sleep(1)
-        convert_to_dng(path)
-
-    if len(photo_paths) != 0:
-        print("Preparing to move dng files.")
-        time.sleep(3)
-
-        move_dngs()
-    else:
-        print("No files where moved.")
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
 
 
 if __name__ == "__main__":
