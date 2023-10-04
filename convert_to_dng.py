446b84b86b63b6d7c6ffd8a6ec9542c062eab83c
Dawson Par <dawson@andrews.edu>
2023-10-04 10:40:59 -0400

    added feature to wait until conversion is finished before continuing loop

--- convert_to_dng.py
+++ convert_to_dng.py
@@ -5,10 +5,23 @@ If dng already exists, skip
 
 import os
 import shutil
+import threading
 import time
+
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 extensions_to_search = [".cr3", ".cr2", ".arw", ".nef"]
 raws_folder_path = "/Users/dawsonpar/Desktop/coding/AU/dng-script/raws"
+new_dng_event = threading.Event()
+
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        print(f"New file created: {event.src_path.split('/')[-1]}")
+        new_dng_event.set()
 
 
 def get_raws(directory_path):
@@ -34,11 +47,11 @@ def get_raws(directory_path):
                 dng_path = os.path.join(raws_folder_path, dng_filename)
 
                 if not os.path.exists(dng_path):
-                    print(file_path)
+                    print(file_path.split("/")[-1])
                     raw_photo_paths.add(file_path)
                 else:
                     print(
-                        f"Skipping {file_path.split('/')[-1]} as it has already been converted."
+                        f"Skipping {file_path.split('/')[-1]} as it's already been converted."
                     )
 
     return raw_photo_paths
@@ -56,20 +69,41 @@ def convert_to_dng(source_path):
     dng_args = "-c"
 
     os.system(open_dng + " " + dng_args + " " + source_path)
-    print(f"Converted {source_path}")
 
 
 def main():
-    print("Running convert_to_dng.py")
-    raw_paths = get_raws(raws_folder_path)
+    event_handler = FileEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=raws_folder_path, recursive=False)
+    observer.start()
 
-    # while len(raw_paths) > 0:
-    for raw in raw_paths:
-        convert_to_dng(raw)
-        time.sleep(1)
-        # raw = raw_paths.pop()
+    try:
+        dng_counter = 0
 
-    pass
+        # print("Running convert_to_dng.py")
+        raw_paths = get_raws(raws_folder_path)
+        number_of_raws = len(raw_paths)
+
+        for raw in raw_paths:
+            convert_to_dng(raw)
+
+            # Wait for new file to be created
+            new_dng_event.wait()
+
+            # Reset event
+            new_dng_event.clear()
+            dng_counter += 1
+
+            if dng_counter == number_of_raws:
+                print("Finished converting all raws")
+                observer.stop()
+                observer.join()
+
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
 
 
 if __name__ == "__main__":
